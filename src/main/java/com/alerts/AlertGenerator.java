a84b06077627d52a15f20d59633a491e86fff318
Ruczak <igor.sarnowski@gmail.com>
2025-05-16 20:57:15 +0200

    Finished alert generator

--- src/main/java/com/alerts/AlertGenerator.java
+++ src/main/java/com/alerts/AlertGenerator.java
@@ -1,16 +1,25 @@
 package com.alerts;
 
+import com.cardio_generator.outputs.OutputStrategy;
 import com.data_management.DataStorage;
 import com.data_management.Patient;
+import com.data_management.PatientRecord;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The {@code AlertGenerator} class is responsible for monitoring patient data
  * and generating alerts when certain predefined conditions are met. This class
  * relies on a {@link DataStorage} instance to access patient data and evaluate
  * it against specific health criteria.
+ * <br/>
+ * ASSUMPTION: We also need an output strategy to notify through appropriate
+ * communication channel.
  */
 public class AlertGenerator {
     private DataStorage dataStorage;
+    private OutputStrategy outputStrategy;
 
     /**
      * Constructs an {@code AlertGenerator} with a specified {@code DataStorage}.
@@ -36,6 +45,26 @@ public class AlertGenerator {
      */
     public void evaluateData(Patient patient) {
         // Implementation goes here
+        List<PatientRecord> records = patient.getRecords(0, System.currentTimeMillis());
+
+        List<AlertChecker> checkers = new ArrayList<>();
+        checkers.add(new BloodPressureAlertChecker());
+        checkers.add(new BloodSaturationAlertChecker());
+        checkers.add(new HypotensiveHypoxemiaAlertChecker());
+        checkers.add(new ECGDataAlertChecker(50));
+        checkers.add(new TriggeredAlertChecker());
+
+        for (PatientRecord record : records) {
+            for (AlertChecker checker : checkers) {
+                checker.checkData(record);
+                for (Alert alert : checker.getQueuedAlerts())
+                    triggerAlert(alert);
+            }
+        }
+    }
+
+    public void setOutputStrategy(OutputStrategy outputStrategy) {
+        this.outputStrategy = outputStrategy;
     }
 
     /**
@@ -43,10 +72,20 @@ public class AlertGenerator {
      * notify medical staff, log the alert, or perform other actions. The method
      * currently assumes that the alert information is fully formed when passed as
      * an argument.
+     * ASSUMPTION: we can use an output strategy to alert the staff through appropriate
+     * communication channel.
      *
      * @param alert the alert object containing details about the alert condition
      */
     private void triggerAlert(Alert alert) {
         // Implementation might involve logging the alert or notifying staff
+        if (outputStrategy == null) return;
+
+        outputStrategy.output(
+                Integer.parseInt(alert.getPatientId()),
+                alert.getTimestamp(),
+                "Alert",
+                alert.getCondition()
+        );
     }
 }
