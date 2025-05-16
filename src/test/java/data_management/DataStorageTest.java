3bc5b05b6bc4dee77e3524d78115c2fd188d287e
Ruczak <igor.sarnowski@gmail.com>
2025-05-16 12:53:24 +0200

    Implemented FileDataReader and DataStorageTest

--- src/test/java/data_management/DataStorageTest.java
+++ src/test/java/data_management/DataStorageTest.java
@@ -1,11 +1,15 @@
 package data_management;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import com.data_management.DataReader;
+import com.data_management.FileDataReader;
 import org.junit.jupiter.api.Test;
 
 import com.data_management.DataStorage;
 import com.data_management.PatientRecord;
 
+import java.io.IOException;
 import java.util.List;
 
 class DataStorageTest {
@@ -14,14 +18,26 @@ class DataStorageTest {
     void testAddAndGetRecords() {
         // TODO Perhaps you can implement a mock data reader to mock the test data?
         // DataReader reader
+        String directory = "test_output";
+
+        FileDataReader reader = new FileDataReader(directory);
 
         //DataStorage storage = new DataStorage(reader);
         DataStorage storage = new DataStorage();
-        storage.addPatientData(1, 100.0, "WhiteBloodCells", 1714376789050L);
-        storage.addPatientData(1, 200.0, "WhiteBloodCells", 1714376789051L);
 
-        List<PatientRecord> records = storage.getRecords(1, 1714376789050L, 1714376789051L);
-        assertEquals(2, records.size()); // Check if two records are retrieved
-        assertEquals(100.0, records.get(0).getMeasurementValue()); // Validate first record
+        try {
+            reader.readData(storage);
+
+            assertEquals(storage.getAllPatients().size(), 6);
+
+            List<PatientRecord> records = storage.getRecords(26, 0, 1747042271984L);
+
+            assertEquals(records.size(), 1);
+            assertEquals(records.get(0).getMeasurementValue(), 154.8512924660576);
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
