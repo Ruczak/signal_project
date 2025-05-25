0344bc80fe1e9188f88df400e1ed966030a5b27c
Ruczak <igor.sarnowski@gmail.com>
2025-05-25 20:13:28 +0200

    Refactored tests and added cleaning up the object before each test

--- src/test/java/data_management/DataReaderTest.java
+++ src/test/java/data_management/DataReaderTest.java
@@ -5,11 +5,14 @@ import com.data_management.FileDataReader;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Field;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -17,38 +20,42 @@ public class DataReaderTest {
     @TempDir
     public Path temp;
 
+    // needed due to sharing memory between test after using 'mvn clean test'
+    @BeforeEach
+    void resetDataStorage() throws NoSuchFieldException, IllegalAccessException {
+        Field f = DataStorage.class.getDeclaredField("instance");
+        f.setAccessible(true);
+        f.set(null, null);
+    }
+
     @Test
     void testFileDataReader() {
-        try {
-            DataStorage storage = DataStorage.getInstance();
+        DataStorage storage = DataStorage.getInstance();
 
-            List<Integer> patientIds = Arrays.asList(43, 26, 17, 24, 31, 44, 2);
+        List<Integer> patientIds = Arrays.asList(43, 26, 17, 24, 31, 44, 2);
 
-            writeToTempFile("Alert.txt", "Patient ID: 43, Timestamp: 1747042264011, Label: Alert, Data: triggered");
-            writeToTempFile("Cholesterol.txt","Patient ID: 26, Timestamp: 1747042261984, Label: Cholesterol, Data: 154.8512924660576");
-            writeToTempFile("DiastolicPressure.txt", "Patient ID: 17, Timestamp: 1747042262029, Label: DiastolicPressure, Data: 75.0");
-            writeToTempFile("SystolicPressure.txt","Patient ID: 24, Timestamp: 1747042261990, Label: SystolicPressure, Data: 123.0");
-            writeToTempFile("Saturation.txt","Patient ID: 31, Timestamp: 1747042261993, Label: Saturation, Data: 96.0%");
-            writeToTempFile("WhiteBloodCells.txt", "Patient ID: 44, Timestamp: 1747042262028, Label: WhiteBloodCells, Data: 8.018321362894895");
-            writeToTempFile("RedBloodCells.txt", "Patient ID: 44, Timestamp: 1747042262033, Label: RedBloodCells, Data: 5.113280609298756");
-            writeToTempFile("ECG.txt", "Patient ID: 2, Timestamp: 1747042261980, Label: ECG, Data: 0.01819583172057701");
+        assertEquals(0, storage.getAllPatients().size());
 
-            FileDataReader reader = new FileDataReader(temp.toString());
+        writeToTempFile("Alert.txt", "Patient ID: 43, Timestamp: 1747042264011, Label: Alert, Data: triggered");
+        writeToTempFile("Cholesterol.txt","Patient ID: 26, Timestamp: 1747042261984, Label: Cholesterol, Data: 154.8512924660576");
+        writeToTempFile("DiastolicPressure.txt", "Patient ID: 17, Timestamp: 1747042262029, Label: DiastolicPressure, Data: 75.0");
+        writeToTempFile("SystolicPressure.txt","Patient ID: 24, Timestamp: 1747042261990, Label: SystolicPressure, Data: 123.0");
+        writeToTempFile("Saturation.txt","Patient ID: 31, Timestamp: 1747042261993, Label: Saturation, Data: 96.0%");
+        writeToTempFile("WhiteBloodCells.txt", "Patient ID: 44, Timestamp: 1747042262028, Label: WhiteBloodCells, Data: 8.018321362894895");
+        writeToTempFile("RedBloodCells.txt", "Patient ID: 44, Timestamp: 1747042262033, Label: RedBloodCells, Data: 5.113280609298756");
+        writeToTempFile("ECG.txt", "Patient ID: 2, Timestamp: 1747042261980, Label: ECG, Data: 0.01819583172057701");
 
-            reader.readData(storage);
+        FileDataReader reader = new FileDataReader(temp.toString());
 
-            assertEquals(7, storage.getAllPatients().size());
-            assertTrue(storage.getAllPatients().stream().allMatch(patient ->
-                    patientIds.contains(patient.getPatientId()) && (
-                            patient.getRecords(0, 1800000000000L).size() == 1 ||
-                            (patient.getRecords(0, 1800000000000L).size() == 2 && patient.getPatientId() == 44)
-                    )
-            ));
-        }
-        catch (IOException e) {
-            System.out.println(e.getMessage());
-            fail();
-        }
+        reader.refresh();
+
+        assertEquals(7, storage.getAllPatients().size());
+        assertTrue(storage.getAllPatients().stream().allMatch(patient ->
+                patientIds.contains(patient.getPatientId()) && (
+                        patient.getRecords(0, 1800000000000L).size() == 1 ||
+                        (patient.getRecords(0, 1800000000000L).size() == 2 && patient.getPatientId() == 44)
+                )
+        ));
     }
 
     private void writeToTempFile(String filename, String data) {
--- src/test/java/data_management/DataStorageTest.java
+++ src/test/java/data_management/DataStorageTest.java
@@ -2,27 +2,39 @@ package data_management;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.data_management.DataStorage;
 import com.data_management.PatientRecord;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 class DataStorageTest {
 
+    // needed due to sharing memory between test after using 'mvn clean test'
+    @BeforeEach
+    void resetDataStorage() throws NoSuchFieldException, IllegalAccessException {
+        Field f = DataStorage.class.getDeclaredField("instance");
+        f.setAccessible(true);
+        f.set(null, null);
+    }
+
     @Test
     void testAddAndGetRecords() {
-
         DataStorage storage = DataStorage.getInstance();
+        int count = storage.getAllPatients().size();
+
         storage.addPatientData(1, 96.0, "Saturation", System.currentTimeMillis());
 
-        assertEquals(storage.getAllPatients().size(), 1);
+        assertEquals(count + 1, storage.getAllPatients().size());
 
         List<PatientRecord> records = storage.getRecords(1, 0, 1800000000000L);
 
-        assertEquals(records.size(), 1);
-        assertEquals(records.get(0).getMeasurementValue(), 96.0);
-
+        assertEquals(1, records.size());
+        assertEquals(96.0, records.get(0).getMeasurementValue());
     }
 }
